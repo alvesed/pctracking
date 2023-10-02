afd813b7ca804d7538d7135771c794892a690756
Edson Francisco Alves <alvesed@gmail.com>
2023-10-02 09:28:54 -0300

    Pctracking resource and docker compose

--- api-pctracking-spring/src/main/java/com/alvesed/apipctrackingspring/entrypoint/rest/dto/PctrackingResponse.java
+++ api-pctracking-spring/src/main/java/com/alvesed/apipctrackingspring/entrypoint/rest/dto/PctrackingResponse.java
@@ -1,7 +1,16 @@
 package com.alvesed.apipctrackingspring.entrypoint.rest.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PctrackingResponse {
 
     private Long idPctracking;
--- api-pctracking-spring/src/main/java/com/alvesed/apipctrackingspring/entrypoint/rest/resources/PctrackingResource.java
+++ api-pctracking-spring/src/main/java/com/alvesed/apipctrackingspring/entrypoint/rest/resources/PctrackingResource.java
@@ -1,24 +1,43 @@
 package com.alvesed.apipctrackingspring.entrypoint.rest.resources;
 
+import com.alvesed.apipctrackingspring.core.domain.Pctracking;
+import com.alvesed.apipctrackingspring.core.usecase.ListPctrakingUseCase;
 import com.alvesed.apipctrackingspring.entrypoint.rest.dto.PctrackingRequest;
 import com.alvesed.apipctrackingspring.entrypoint.rest.dto.PctrackingResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/pctracking")
 @RequiredArgsConstructor
 public class PctrackingResource {
 
+    private final ListPctrakingUseCase listPctrakingUseCase;
+
     @GetMapping
     public List<PctrackingResponse> listRequests() {
-        return null;
+        System.out.println("Listing requests");
+        var listPctracking = listPctrakingUseCase.execute();
+
+        return toListPctrackingResponse(listPctracking);
+        
+    }
+
+    private List<PctrackingResponse> toListPctrackingResponse(List<Pctracking> listPctracking) {
+
+        return listPctracking.stream()
+                .map(pctracking ->
+                        new PctrackingResponse(pctracking.getIdPctracking(), pctracking.getDateTimeRequestTracking()))
+                .collect(Collectors.toList());
     }
 
     @PostMapping
     public PctrackingResponse createRequest(@RequestBody PctrackingRequest request) {
+        System.out.println("Create request");
         return null;
     }
 
